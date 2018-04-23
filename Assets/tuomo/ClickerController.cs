1c0468ef9401a2e61477aa145eb33e934f2eb8a9
tuomo <tuomo@superplusgames.com>
2018-04-23 22:42:01 +0300

    Elevator scene with simple game loop

---
+++ Assets/tuomo/ClickerController.cs
@@ -0,0 +1,233 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickerController : MonoBehaviour {
+
+    enum PlayerState { Active, Moving, Inactive, DoorsOpening }
+    PlayerState _currentState;
+
+    [Header("Difficulty settings")]
+    [Space(5)]
+    public Slider healthSlider;
+    public float startHealth = 5f;
+    public float target = 100f;
+    public int floors = 3;
+    public float[] depletionRates;
+    public float clickPower = 2f;
+
+    [Header("Elevator specs")]
+    [Space(5)]
+    public GameObject leftDoor;
+    public GameObject rightDoor;
+    public float moveDistance = 7f;
+    public float waitTime = 5f;
+
+    [Header("Enemies")]
+    [Space(5)]
+    public GameObject enemyPrefab;
+    public List<GameObject> enemies = new List<GameObject>();
+    public float enemyStrength = 2f;
+
+    [Header("Misc")]
+    [Space(5)]
+    public Text floorText;
+    public Text rateText;
+
+    Vector3 leftDoorStart;
+    Vector3 rightDoorStart;
+    float _currentDistance = 0f;
+    float _currentHealth;
+    float _moveTimeLeft;
+    int _currentFloor = 1;
+    float _enemyDepletionRate;
+
+	void Start () {
+        if (depletionRates.Length != floors)
+        {
+            Debug.LogError("Floor amount does not match with depletion rates.");
+        }
+
+        SetFloorText(_currentFloor);
+
+        _currentState = PlayerState.Active;
+        _currentHealth = startHealth;
+        healthSlider.minValue = 0;
+        healthSlider.maxValue = target;
+
+        leftDoorStart = leftDoor.transform.position;
+        rightDoorStart = rightDoor.transform.position;
+
+        AdjustHealthSlider();
+	}
+	
+	void Update () {
+
+        switch (_currentState)
+        {
+            case PlayerState.Active:
+
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    PushButton();
+                }
+
+                ReduceHealth();
+                AdjustFloors(_currentHealth);
+
+                print("Player active");
+                break;
+
+            case PlayerState.Inactive:
+                print("Player inactive");
+                break;
+
+            case PlayerState.Moving:
+                AdjustFloors(target);
+                MoveElevator();
+                print("Elevator moving.");
+                break;
+
+            case PlayerState.DoorsOpening:
+                AdjustFloors(0f);
+                print("Doors opening.");
+                break;
+
+            default:
+                print("Default state");
+                break;
+        }
+
+        // DEBUG ENEMY SPAWNER
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            GameObject g = Instantiate(enemyPrefab,
+                                       new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f), Random.Range(-3f, 3f)),
+                                       Quaternion.identity);
+            AddEnemy(g);
+        }
+
+        // DEBUG
+        rateText.text = "Depletion rate: " + (depletionRates[_currentFloor - 1] + _enemyDepletionRate).ToString();
+    }
+
+    void AdjustHealthSlider()
+    {
+        healthSlider.value = _currentHealth;
+    }
+
+    void ReduceHealth()
+    {
+        _currentHealth -= (depletionRates[_currentFloor - 1] + _enemyDepletionRate) * Time.deltaTime;
+        AdjustHealthSlider();
+        if(_currentHealth < 0)
+        {
+            _currentHealth = 0;
+            // Lose condition?
+
+            //_currentState = PlayerState.Inactive;
+            //print("Player died!");
+        }
+    }
+
+    void PushButton()
+    {
+        _currentHealth += clickPower;
+        AdjustHealthSlider();
+
+        if(_currentHealth >= target)
+        {
+            print("Floor " + _currentFloor + " finished.");
+            RemoveAllEnemies();
+            AdjustFloors(target);
+            LevelFinished();
+        }
+    }
+
+    void AdjustFloors(float newPosition)
+    {
+        Vector3 leftNewPos = leftDoorStart + new Vector3(newPosition / target * moveDistance, 0, 0);
+        Vector3 rightNewPos = rightDoorStart - new Vector3(newPosition / target * moveDistance, 0, 0);
+        var smooth = 5f;
+
+        leftDoor.transform.position = Vector3.Lerp(leftDoor.transform.position, leftNewPos, Time.deltaTime * smooth);
+        rightDoor.transform.position = Vector3.Lerp(rightDoor.transform.position, rightNewPos, Time.deltaTime * smooth);
+
+        if (_currentState == PlayerState.DoorsOpening)
+        {
+            if(Vector3.Distance(leftDoor.transform.position, leftDoorStart) < 0.1f && Vector3.Distance(rightDoor.transform.position, rightDoorStart) < 0.1f)
+            {
+                _currentFloor++;
+                SetFloorText(_currentFloor);
+                if(_currentFloor > floors)
+                {
+                    Debug.LogError("Win condition reached.");
+                    _currentState = PlayerState.Inactive;
+                }
+                else
+                {
+                    print("Floor " + _currentFloor + " starting.");
+                    _currentState = PlayerState.Active;
+                    _currentHealth = startHealth;
+                }
+            }
+        }
+    }
+
+    void MoveElevator()
+    {
+        _moveTimeLeft -= Time.deltaTime;
+        if(_moveTimeLeft < 0)
+        {
+            OpenDoors();
+        }
+    }
+
+    void OpenDoors()
+    {
+        _currentState = PlayerState.DoorsOpening;
+    }
+
+    void LevelFinished()
+    {
+        _moveTimeLeft = waitTime;
+        _currentState = PlayerState.Moving;
+    }
+
+    void SetFloorText(int floorNumber)
+    {
+        if (floorNumber <= floors)
+        {
+            floorText.text = floorNumber.ToString();
+        }
+        else
+        {
+            floorText.text = "Penthouse";
+        }
+    }
+
+    void RemoveAllEnemies()
+    {
+        foreach(GameObject enemy in enemies)
+        {
+            //enemies.Remove(enemy);
+            Destroy(enemy);
+        }
+        _enemyDepletionRate = 0;
+    }
+
+    public void AddEnemy(GameObject enemyAdded)
+    {
+        enemies.Add(enemyAdded);
+        _enemyDepletionRate += enemyStrength;
+    }
+
+    //public void RemoveEnemy(GameObject enemyRemoved)
+    //{
+    //    enemies.Remove(enemyRemoved);
+    //    Destroy(enemyRemoved);
+    //    _enemyDepletionRate -= enemyStrength;
+    //}
+
+}
